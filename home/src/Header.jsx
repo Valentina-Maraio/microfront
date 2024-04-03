3d2191827898d4d9c1a97621b44f4a9f2aee0300
Valentina-Maraio <valentina.maraio89@gmail.com>
2024-04-03 18:04:34 +0200

    managing errors

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
         <>
             <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
                 <h2>Home page</h2>
+                <p>{app.name}</p>
             </div>
         </>
     )
